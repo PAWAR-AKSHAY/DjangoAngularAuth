0243b3a51b3ab463adf8e812a23d4330d80ed300
PAWAR-AKSHAY <pawar.pawar1@gmail.com>
2022-09-30 16:44:13 +0530

    - Adds User Model - Adds Register and Login api - Adds requirements.txt

--- Backend/core/views.py
+++ Backend/core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import exceptions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from core import models, serializers
+
+
+class RegisterAPIView(APIView):
+
+    def post(self, request):
+        data = request.data
+        if data["password"] != data["password_confirm"]:
+            raise exceptions.APIException("Password do not match!")
+        
+        serializer = serializers.UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data["email"]
+        password = request.data["password"]
+
+        user = models.User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed("Invalid Credentials")
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed("Invalid Credentials")
+        
+        serializer = serializers.UserSerializer(user)
+        return Response(serializer.data)
