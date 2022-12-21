aa9bbd18237f09914aebad3e451a21baee6f1cb5
PAWAR-AKSHAY <pawar.pawar1@gmail.com>
2022-12-21 12:41:35 +0530

    Frontend - Add bootstrap cdn - Add login and registration form - Add nav component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
